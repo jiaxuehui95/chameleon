e167fb67796f4b8add1f36e10e0b334c411d3ff9
Xuehui JIA <jiaxuehui95@gmail.com>
2018-02-19 16:15:21 +0100

    init project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(500,500);
+    document.getElementById('defaultCanvas0')
+    $('defaultCanvas0').addClass()
+}
+
+function draw() {
+    background(255,0,255);
+}
